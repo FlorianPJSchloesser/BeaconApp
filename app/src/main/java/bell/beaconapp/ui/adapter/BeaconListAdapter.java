f8656ef778f6dd738222f3bb944b09d4cfade2dc
Floschlo <florianpjschloesser@gmail.com>
2017-02-08 23:03:57 +0100

    Cleaned adapter

--- app/src/main/java/bell/beaconapp/ui/adapter/BeaconListAdapter.java
+++ app/src/main/java/bell/beaconapp/ui/adapter/BeaconListAdapter.java
@@ -3,6 +3,7 @@ package bell.beaconapp.ui.adapter;
 /**
  * Created by Florian Schlösser on 08.02.2017.
  */
+
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -16,33 +17,65 @@ import bell.beaconapp.R;
 import bell.beaconapp.model.Beacon;
 
 
-public class BeaconListAdapter extends RecyclerView.Adapter<BeaconListAdapter.BeaconListViewHolder>{
+public class BeaconListAdapter extends RecyclerView.Adapter<BeaconListAdapter.BeaconListViewHolder> {
 
-    Context mContext;
+    /**
+     * Context used to inflate layouts.
+     */
+    private Context mContext;
+
+    /**
+     * Stores the expanded position.<br/>-1 when no item is expanded.
+     */
     int mExpandedPosition;
-    RecyclerView mRecyclerView;
+
+    /**
+     * Data structure holding the beacons.
+     */
     ArrayList<Beacon> mDataSet;
 
-    public BeaconListAdapter(Context context, RecyclerView recyclerView) {
+    /**
+     * Creates BeaconListAdapter for the use with RecyclerView.
+     *
+     * @param context Used to inflate layouts.
+     */
+    public BeaconListAdapter(Context context) {
         mContext = context;
         mExpandedPosition = -1;
-        mRecyclerView = recyclerView;
         mDataSet = new ArrayList<>();
     }
 
+    /**
+     * Adds beacon to the adapter.
+     *
+     * @param beacon Beacon to add.
+     */
     public void add(Beacon beacon) {
-        mDataSet.add(beacon);
-        notifyItemInserted(mDataSet.size() - 1);
+        if (beaconAlreadyExist(beacon)) {
+            int beaconIndex = getIndexOfBeacon(beacon);
+            mDataSet.remove(beacon);
+            mDataSet.add(beaconIndex, beacon);
+            notifyItemChanged(beaconIndex);
+        } else {
+            mDataSet.add(beacon);
+            notifyItemInserted(mDataSet.size() - 1);
+        }
     }
 
-    public void remove(int pos) {
-        mDataSet.remove(pos);
-        notifyItemRemoved(pos);
+    /**
+     * Removes beacon from adapter.
+     *
+     * @param beacon Beacon to remove.
+     */
+    public void remove(Beacon beacon) {
+        int beaconIndex = getIndexOfBeacon(beacon);
+        mDataSet.remove(beaconIndex);
+        notifyItemRemoved(beaconIndex);
     }
 
     @Override
     public BeaconListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(mContext).inflate(R.layout.list_beacon,null);
+        View view = LayoutInflater.from(mContext).inflate(R.layout.list_beacon, null);
         return new BeaconListViewHolder(view);
     }
 
@@ -54,10 +87,10 @@ public class BeaconListAdapter extends RecyclerView.Adapter<BeaconListAdapter.Be
     @Override
     public void onBindViewHolder(final BeaconListViewHolder holder, final int position) {
 
-        final boolean isExpanded = position==mExpandedPosition;
+        final boolean isExpanded = position == mExpandedPosition;
 
-        holder.mDetails.setVisibility(isExpanded?View.VISIBLE:View.GONE);
-        holder.mList.setVisibility(isExpanded?View.GONE:View.VISIBLE);
+        holder.mDetails.setVisibility(isExpanded ? View.VISIBLE : View.GONE);
+        holder.mList.setVisibility(isExpanded ? View.GONE : View.VISIBLE);
         holder.itemView.setActivated(isExpanded);
         holder.applyBeacon(mDataSet.get(position));
         holder.mClickableView.setOnClickListener(new View.OnClickListener() {
@@ -69,7 +102,20 @@ public class BeaconListAdapter extends RecyclerView.Adapter<BeaconListAdapter.Be
         });
     }
 
-    public class BeaconListViewHolder extends RecyclerView.ViewHolder{
+    private int getIndexOfBeacon(Beacon beacon) {
+        for (int i = 0; i < mDataSet.size(); i++) {
+            if (beacon.getUUID().equals(mDataSet.get(i).getUUID())) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    private boolean beaconAlreadyExist(Beacon beacon) {
+        return false;
+    }
+    
+    public class BeaconListViewHolder extends RecyclerView.ViewHolder {
 
         public View mClickableView;
         public View mDetails;
@@ -89,7 +135,7 @@ public class BeaconListAdapter extends RecyclerView.Adapter<BeaconListAdapter.Be
         public void applyBeacon(Beacon beacon) {
             mListTitle.setText(beacon.getTitle());
             mDetailTitle.setText(beacon.getTitle());
-            
+
         }
     }
 }
